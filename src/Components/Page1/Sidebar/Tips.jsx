b6fc3ae9d61061f6a3a8bed425d52142510c9774
Thanawit Narumonnantakhan <mingthanawit08@gmail.com>
2022-08-22 22:32:52 +0700

    add href tip

--- src/Components/Page1/Sidebar/Tips.jsx
+++ src/Components/Page1/Sidebar/Tips.jsx
@@ -6,6 +6,7 @@ const Tips = () => {
     return (
         <>
             <div className='Tip-container'>
+                <a href='https://www.healthline.com/nutrition/how-to-eat-healthy-guide' target="_blank">
                 <div className='tip-text-container'>
                     <img src={tiptop} /> 
                     <h3>Tips of the day</h3>
@@ -13,13 +14,13 @@ const Tips = () => {
                 <div className='tip-data-container'>
                     <div className='tip-img-container'>
                         <img src={tip} /> 
-                        {/* for mock up */}
                     </div>
                     <div className='tip-result-container'>
                         <h6>The Definitive Guide to Healthy Eating in Real Life</h6>
                         <p>Depending on whom you ask, “healthy eating” may take any number of forms. It seems that everyone</p>
                     </div>
                 </div>
+                </a>
             </div>
         </>
     )
